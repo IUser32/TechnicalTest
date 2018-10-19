1ce1275fdfc9203a64a4c11da159256db8858b3e
IUser32 <javier-uribe@hotmail.com>
2018-10-19 00:55:14 -0400

    .

--- backend/models.py
+++ backend/models.py
@@ -10,7 +10,7 @@ class Movie(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
-        return self.title
+        return self.title 
 
 class Client(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
